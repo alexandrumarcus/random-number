15ca5907a85976ca80c76d73ed4a978cb9c5f232
llsan <51889604+alexandrumarcus@users.noreply.github.com>
2019-10-12 03:32:40 +0300

    first text

--- random.js
+++ random.js
@@ -1,3 +1,24 @@
-let a=5;
-console.log('a');
+class BBS {
+    constructor(p, q, seed) {
+        this.p = p;
+        this.q = q;
+        this.x0 = seed;
+        this.n = p * q;
+    }
 
+    getRandNumb() {
+        var nextRandNumb = (this.x0 * this.x0) % this.n;
+        this.x0 = nextRandNumb;
+        return nextRandNumb;
+    }
+
+    getRandBit() {
+        return this.getRandNumb() % 2;
+    }
+}
+
+let p = 11, q = 19, s = 5;
+bbs = new BBS(p, q, s);
+for (var i = 0; i < 6; i++) {
+    console.log(bbs.getRandNumb());
+}
